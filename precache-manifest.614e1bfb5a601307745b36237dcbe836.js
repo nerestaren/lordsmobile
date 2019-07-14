2a0b7071dc02cdcfc1ebf72bc0c227f43c558584
Antoni Oliver Tomàs <antoni.oliver.tomas@gmail.com>
2019-07-14 14:16:07 +0200

    Updates

---
+++ precache-manifest.614e1bfb5a601307745b36237dcbe836.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2570dde0cea5f12e37779c58ef8eff28",
+    "url": "/lordsmobile/index.html"
+  },
+  {
+    "revision": "9eb6e0416cf996c64e44",
+    "url": "/lordsmobile/static/css/2.885429ea.chunk.css"
+  },
+  {
+    "revision": "f6ccb5bc7cf77d646e78",
+    "url": "/lordsmobile/static/css/main.82cb4437.chunk.css"
+  },
+  {
+    "revision": "9eb6e0416cf996c64e44",
+    "url": "/lordsmobile/static/js/2.eb9bbfea.chunk.js"
+  },
+  {
+    "revision": "f6ccb5bc7cf77d646e78",
+    "url": "/lordsmobile/static/js/main.25680ac7.chunk.js"
+  },
+  {
+    "revision": "799c5930eb3a917841a8",
+    "url": "/lordsmobile/static/js/runtime~main.be4368d0.js"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/lordsmobile/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/lordsmobile/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/lordsmobile/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/lordsmobile/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/lordsmobile/static/media/glyphicons-halflings-regular.fa277232.woff"
+  }
+]);
